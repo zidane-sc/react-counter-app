7eded2be24036e0767b932c4ebdc7a4f22fb9409
zidanesc <zidankren02@gmail.com>
2022-04-02 23:37:03 +0700

    Simple counter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>{ count }</h1>
+
+      <button onClick={ () => setCount(count + 1)}>Plus</button>
+      <button onClick={ () => setCount(count - 1)}>Minus</button>
     </div>
   );
 }
